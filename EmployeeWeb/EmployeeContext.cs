a92c09bcf9ce09ed130078923685094aa51df6d2
rrazvan94 <rrazvan94@gmail.com>
2016-04-04 18:32:37 +0300

    Initial Commit

---
+++ EmployeeWeb/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext() : base("name=EmployeeDbConnectionString")
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeWeb/Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; } 
+    }
+}
